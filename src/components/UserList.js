cb45d3943bd976b731613ad77b9c0d64646e2359
ananyapoojary <ananyaarunpoojary@gmail.com>
2025-02-20 09:02:45 +0530

    yet another commit

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,22 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from '@apollo/client';
 import { GET_USERS } from '../graphql/queries';
 import styles from '../styles/UserList.module.css';
 
 const UserList = () => {
-  const { loading, error, data } = useQuery(GET_USERS);
+  const [showUsers, setShowUsers] = useState(false);
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
+  // useQuery with refetch function
+  const { loading, error, data, refetch } = useQuery(GET_USERS, {
+    skip: !showUsers, // Skip query until showUsers is true
+  });
+
+  const handleDisplayUsers = () => {
+    setShowUsers(true); // Trigger the users display on button click
+    refetch(); // Manually refetch users each time the button is clicked
+  };
 
   return (
-    <ul className={styles.userList}>
-      {data.getAllUsers.map(user => (
-        <li key={user.id} className={styles.userItem}>
-          {user.name} ({user.email})
-        </li>
-      ))}
-    </ul>
+    <div>
+      <button onClick={handleDisplayUsers} className={styles.displayButton}>
+        Display Users
+      </button>
+
+      {showUsers && (
+        <>
+          {loading && <p>Loading...</p>}
+          {error && <p>Error: {error.message}</p>}
+          {data && (
+            <ul className={styles.userList}>
+              {data.getAllUsers.map(user => (
+                <li key={user.id} className={styles.userItem}>
+                  {user.name} ({user.email})
+                </li>
+              ))}
+            </ul>
+          )}
+        </>
+      )}
+    </div>
   );
 };
 
